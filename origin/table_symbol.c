701bdddd4487f2f8805358dd461ce2adea4dc314
Yunan HOU <yunan8@outlook.com>
2021-04-07 08:11:07 +0200

    table symbol

--- origin/table_symbol.c
+++ origin/table_symbol.c
@@ -6,16 +6,14 @@
 
 //initialise symbol table
 
-struct table
-{
+struct table{
     int size;
     struct symbol symbols[500];
 };
 
 struct table map = {0};
 
-int push_symbol(const char *id, int depth, int constant, int pointer, int func_id)
-{
+int push_symbol(const char *id, int depth, int constant, int pointer, int func_id){
 	//trouver si le symbole déjà existe
 	if(find_symbol(id,depth,func_id)==-1){
 		struct symbol *new = &(map.symbols[map.size]);
@@ -28,14 +26,13 @@ int push_symbol(const char *id, int depth, int constant, int pointer, int func_i
 		map.size++;
 		return map.size - 1;
 	}else{
-		printf("Error syntaxique !");
+		printf("Error syntaxique : Double declaration\n");
 		exit(1);
 	}
    
 }
 
-int find_symbol(const char *id, int depth, int func_id)
-{
+int find_symbol(const char *id, int depth, int func_id){
     for (int i = 0; i < map.size; i++)
     {
         struct symbol *current = &(map.symbols[i]);
@@ -46,3 +43,75 @@ int find_symbol(const char *id, int depth, int func_id)
     return -1;
 }
 
+void pop_symbol(){
+	map.size--;
+}
+
+int get_last_index(){
+	return map.size - 1;
+}
+
+void set_initialized(const char* id, int depth,int func_id){
+	int adr = find_symbol(id,depth,func_id);
+	map.symbols[adr].initialise = 1;
+}
+int is_initialized(const char *id, int depth, int func_id){
+    int adr = find_symbol(id, depth, func_id);
+    return symbol_table.symbols[adr].initialized;
+}
+
+int is_const(const char *id, int depth, int func_id){
+    int adr = find_symbol(id, depth, func_id);
+    return symbol_table.symbols[adr].constant;
+}
+
+int is_pointer(int index){
+  return symbol_table.symbols[index].pointer;
+}
+
+void clear_symbols_by_func(int func_id){
+  int fin = 0;
+  while(fin == 0) {
+      struct symbol *last = &(symbol_table.symbols[symbol_table.size - 1]);
+      if (last->function_id == func_id) {
+        pop_symbol();
+        if(symbol_table.size == 0) {
+          fin = 1;
+        }
+      } else {
+        fin = 1;
+      }
+  }
+}
+
+void clear_symbols_by_depth(int depth){
+  int fin = 0;
+  while(fin == 0) {
+      struct symbol *last = &(symbol_table.symbols[symbol_table.size - 1]);
+      if (last->depth == depth) {
+        pop_symbol();
+        if(symbol_table.size == 0) {
+          fin = 1;
+        }
+      } else {
+        fin = 1;
+      }
+  }
+}
+
+void display_table(){
+    printf("Symbol table :\n");
+    for (int i = 0; i < symbol_table.size; i++)
+    {
+        struct symbol *current = &(symbol_table.symbols[i]);
+        int depth = current->depth;
+        printf("index=%d\t", i);
+        printf("id=%s\tconst=%d\tinit=%d\tdepth=%d\tfunc_id=%d\tpointer=%d\n",
+          current->id, current->constant,
+          current->initialized, current->depth,
+          current->function_id,
+          current->pointer);
+    }
+    printf("\n");
+}
+)
